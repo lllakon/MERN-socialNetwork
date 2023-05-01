ebae24f87683f9ee09f3be6caa3b906a10b321cd
lllakon <CoffeWhite58@gmail.com>
2023-05-01 18:55:38 +0200

    Комментарии обновляются при добавлении нового

--- frontend-fullstack-blog/src/components/AddComment/index.jsx
+++ frontend-fullstack-blog/src/components/AddComment/index.jsx
@@ -8,20 +8,21 @@ import TextField from '@mui/material/TextField'
 import Avatar from '@mui/material/Avatar'
 import Button from '@mui/material/Button'
 
-export const AddComment = ({ postId, userData }) => {
-	const [newComment, setNewComment] = useState('')
+export const AddComment = ({ postId, userData, setNewComment }) => {
+	const [inputValue, setInputValue] = useState('')
 
 	const sendCommentHandler = async () => {
 		const newCommentData = {
-			text: newComment,
+			text: inputValue,
 			userId: userData._id,
 			fullName: userData.fullName,
 			avatarUrl: userData.avatarUrl,
 		}
+		setNewComment(prevItems => [...prevItems, newCommentData])
 
 		try {
 			await axios.patch(`/comments/${postId}`, newCommentData)
-			setNewComment('')
+			setInputValue('')
 		} catch (error) {
 			console.warn(error)
 			alert('Не удалось отправить комментарий')
@@ -34,8 +35,8 @@ export const AddComment = ({ postId, userData }) => {
 				<Avatar classes={{ root: styles.avatar }} src={userData.avatarUrl} />
 				<div className={styles.form}>
 					<TextField
-						value={newComment}
-						onChange={(e) => setNewComment(e.target.value)}
+						value={inputValue}
+						onChange={(e) => setInputValue(e.target.value)}
 						label='Написать комментарий'
 						variant='outlined'
 						maxRows={10}
--- frontend-fullstack-blog/src/components/CommentsBlock.jsx
+++ frontend-fullstack-blog/src/components/CommentsBlock.jsx
@@ -1,7 +1,10 @@
 import React, { useEffect, useState } from 'react'
+import { useSelector } from 'react-redux'
 import axios from '../axios'
 
 import { SideBlock } from './SideBlock'
+import { AddComment } from './AddComment'
+
 import ListItem from '@mui/material/ListItem'
 import ListItemAvatar from '@mui/material/ListItemAvatar'
 import Avatar from '@mui/material/Avatar'
@@ -9,13 +12,12 @@ import ListItemText from '@mui/material/ListItemText'
 import Divider from '@mui/material/Divider'
 import List from '@mui/material/List'
 import Skeleton from '@mui/material/Skeleton'
-import { AddComment } from './AddComment'
-import { useSelector } from 'react-redux'
 
 export const CommentsBlock = ({ postId }) => {
 	const userData = useSelector((state) => state.auth.data)
 	const [isLoading, setLoading] = useState(true)
 	const [comments, setComments] = useState([])
+	const [newComment, setNewComment] = useState([])
 
 	useEffect(() => {
 		axios
@@ -28,7 +30,7 @@ export const CommentsBlock = ({ postId }) => {
 				console.log(err)
 				alert('Ошибка получения комментариев')
 			})
-	}, [])
+	}, [newComment])
 
 	return (
 		<SideBlock title='Комментарии'>
@@ -57,7 +59,7 @@ export const CommentsBlock = ({ postId }) => {
 				))}
 			</List>
 			{userData ? (
-				<AddComment postId={postId} userData={userData} />
+				<AddComment postId={postId} userData={userData} setNewComment={setNewComment} />
 			) : (
 				<p style={{ textAlign: 'center', padding: '30px 0 45px 0' }}>
 					Войдите или зарегестрируйтесь чтобы оставлять комментарии

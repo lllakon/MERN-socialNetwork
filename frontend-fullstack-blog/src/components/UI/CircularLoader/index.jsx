d34250c436fc3bbcbc0c194de6fbef509791ae93
lllakon <CoffeWhite58@gmail.com>
2023-05-13 19:39:58 +0200

    Доработка лоадера

--- frontend-fullstack-blog/src/components/UI/CircularLoader/index.jsx
+++ frontend-fullstack-blog/src/components/UI/CircularLoader/index.jsx
@@ -1,9 +1,9 @@
 import styles from './CircularLoader.module.scss'
 
-export const CircularLoader = () => {
+export const CircularLoader = ({ fullSize }) => {
 	return (
-		<div className={styles.container}>
-			<span className={styles.spinner + ' ' + styles.spinnerQuarter}></span>
+		<div className={`${styles.container} ${fullSize && styles.fullSize}`}>
+			<span className={`${styles.spinner} ${styles.spinnerQuarter}`}></span>
 		</div>
 	)
 }
